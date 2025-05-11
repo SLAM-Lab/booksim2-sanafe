63077ab7280ebe28d1cf2ff3d7422a8d4ce23884
James Boyle <james.boyle@utexas.edu>
2025-05-11 16:32:00 -0500

    Add library routines to push message events
    The interface needs ways to pass external message info in,
    ideally avoiding the need for SANA-FE to be aware of the Booksim
    message struct.

--- src/booksim_lib.hpp
+++ src/booksim_lib.hpp
@@ -34,6 +34,8 @@ struct SpikeEvent;
 
 void booksim_init();
 BookSimConfig booksim_load_config(int argc, char **argv);
+void booksim_create_spike_event(int timestep, int src_neuron, std::pair<int, int> dest_hw, double generation_latency );
+void booksim_create_processing_event(int timestep, int src_neuron, std::pair<int, int> dest_hw, double generation_latency, double processing_latency );
 bool booksim_run(BookSimConfig const & config, std::vector<SpikeEvent> spike_events = {});
 void booksim_close();
 
